b88768d0f7e89c15face8940ef762dfb11ebde97
Carlo Martinez <fz1958@wayne.edu>
2018-10-07 14:11:24 -0400

    Added CRC response in GET method, and staged a POST Outline

--- twitter.go
+++ twitter.go
@@ -1,13 +1,43 @@
 package TwitterIOT
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
 
+//Twitter Webhook
+type testStruct struct {
+	Test string
+}
+
 func twitterhook(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
+	//Log that this function is called
 	log.Debugf(ctx, "Webhook success!")
+
+	switch r.Method {
+	//If requesting a "Get" run CRC
+	case http.MethodGet:
+		log.Debugf(ctx, "This is a Get method")
+		secret := "8LRLAFoS7FlUE6q2hJKMG2kbtBXLPUTyl6btc1PABayI3416IV"
+		message := r.URL.Query().Get("message")
+		GetCRCResponse(secret, message, w)
+
+		//If post parse data
+	case http.MethodPost:
+		log.Debugf(ctx, "This is a Post method")
+		decoder := json.NewDecoder(r.Body)
+		var t testStruct
+		err := decoder.Decode(&t)
+		if err != nil {
+			panic(err)
+		}
+		log.Debugf(ctx, t.Test)
+
+	default: //Else this gets awkward
+		log.Debugf(ctx, "Yikes this wasn't supposed to happen (Not a GET or POST)")
+	}
 }
